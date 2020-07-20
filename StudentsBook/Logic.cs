faa4fdc7f6de730284be3bab86a0ce19f82489d3
basarab1504 <joker-power@yandex.ru>
2020-07-20 20:00:07 +0300

    Initial commit

---
+++ StudentsBook/Logic.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace StudentsBook
+{
+    [XmlType("Student")]
+    public class Student : INotifyPropertyChanged
+    {
+        [XmlElement("Name")]
+        public string Name { get; set; }
+        [XmlElement("Payment")]
+        public string Payment { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+
+    public class Subject
+    {
+        public Student Student { get; set; }
+        public Homework Homework { get; set; }
+        public bool IsPaid { get; set; }
+    }
+
+    public class Homework
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Code { get; set; }
+    }
+
+    public static class Formatter
+    {
+        public static IEnumerable<Student> G()
+        {
+            List<Student> s = new List<Student>();
+            using (var reader = new StreamReader("students.xml"))
+            {
+                XmlSerializer deserializer = new XmlSerializer(typeof(List<Student>),
+                    new XmlRootAttribute("Students"));
+                s = (List<Student>)deserializer.Deserialize(reader);
+            }
+            return s;
+        }
+
+        public static T DeserializeXMLFileToObject<T>(string XmlFilename)
+        {
+            T returnObject = default(T);
+            if (string.IsNullOrEmpty(XmlFilename)) return default(T);
+            try
+            {
+                StreamReader xmlStream = new StreamReader(XmlFilename);
+                XmlSerializer serializer = new XmlSerializer(typeof(T));
+                returnObject = (T)serializer.Deserialize(xmlStream);
+            }
+            catch (Exception ex)
+            {
+                //ExceptionLogger.WriteExceptionToConsole(ex, DateTime.Now);
+            }
+            return returnObject;
+        }
+    }
+}
---
+++ StudentsBook/StudentsViewModel.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace StudentsBook
+{
+    public class RelayCommand : ICommand
+    {
+        private Action<object> execute;
+        private Func<object, bool> canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return this.canExecute == null || this.canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            this.execute(parameter);
+        }
+    }
+
+    class StudentsViewModel : INotifyPropertyChanged
+    {
+        private Student selectedStudent;
+
+        public ObservableCollection<Student> Students { get; set; }
+
+        private RelayCommand add;
+        public RelayCommand Add
+        {
+            get
+            {
+                return add ??
+                  (add = new RelayCommand(obj =>
+                  {
+                      Student student = new Student() { Name = "Новичок" };
+                      Students.Add(student);
+                      SelectedStudent = student;
+                  }));
+            }
+        }
+
+    public Student SelectedStudent
+        {
+            get { return selectedStudent; }
+            set
+            {
+                selectedStudent = value;
+                OnPropertyChanged("SelectedStudent");
+            }
+        }
+
+        public StudentsViewModel()
+        {
+            Students = new ObservableCollection<Student>(Formatter.G());
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string prop )
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
